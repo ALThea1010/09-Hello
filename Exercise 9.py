d2d9f4ee508197979d953c2e3a435f9a71b07b35
ALThea1010 <164994153+ALThea1010@users.noreply.github.com>
2024-10-21 12:18:28 +0400

    Add files via upload

---
+++ Exercise 9.py
@@ -0,0 +1,13 @@
+"""
+Assessment1-Programming-Skills-Portfolio Exercise 9: Hello
+"""
+
+
+def hello():
+    print("Hello")  # Prints "Hello"
+
+def main():
+    hello()  # Call the hello() function
+
+if __name__ == "__main__":
+    main()
